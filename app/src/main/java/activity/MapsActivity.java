49e07542307c2141815d84875ce584c2ebaa6a70
Nourhan Zidan <nourhanzidan98@gmail.com>
2020-11-03 23:10:21 +0200

    Null pointer exception in MapsActivity (Monday at uni) Registration and Login are okay.

--- app/src/main/java/activity/MapsActivity.java
+++ app/src/main/java/activity/MapsActivity.java
@@ -1,12 +1,14 @@
 package activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -22,109 +24,55 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-//
-//public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-//    Location currentLocation;
-//    FusedLocationProviderClient fusedLocationProviderClient;
-//    private static final int REQUEST_CODE = 101;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-//        fetchLocation();
-//    }
-//    private void fetchLocation() {
-//        if (ActivityCompat.checkSelfPermission(
-//                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            Log.d("jj" , "All permissions granted") ;
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-//            return;
-//        }
-//        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-//            @Override
-//            public void onSuccess(Location location) {
-//                if (location != null) {
-//                    currentLocation = location;
-//                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-//                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//                    assert supportMapFragment != null;
-//                    supportMapFragment.getMapAsync(MapsActivity.this);
-//                }
-//            }
-//        });
-//    }
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-//        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
-//        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-//        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
-//        googleMap.addMarker(markerOptions);
-//    }
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        switch (requestCode) {
-//            case REQUEST_CODE:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    fetchLocation();
-//                }
-//                break;
-//        }
-//    }
-//}
+public class MapsActivity extends AppCompatActivity{
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    Location currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    private static final int REQUEST_CODE = 101;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
-    }
-    private void fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
+        setContentView(R.layout.activity_maps);
+
+        supportMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if(ActivityCompat.checkSelfPermission(MapsActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            getCurrentLocation();
         }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        else{
+            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+    }
+
+    private void getCurrentLocation() {
+        Task<Location> task =client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
-                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-                    assert supportMapFragment != null;
-                    supportMapFragment.getMapAsync(MapsActivity.this);
+            public void onSuccess(final Location location) {
+                if(location!=null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I am here");
+
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                            googleMap.addMarker(options);
+                        }
+                    });
                 }
             }
         });
     }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
-        googleMap.addMarker(markerOptions);
-    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    fetchLocation();
-                }
-                break;
+        if(requestCode==44){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
         }
     }
 }
--- app/src/main/java/app/AppConfig.java
+++ app/src/main/java/app/AppConfig.java
@@ -2,8 +2,8 @@ package app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.43.159/android_api/login.php";
+    public static String URL_LOGIN = "http://192.168.1.11/android_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.43.159/android_api/register.php";
+    public static String URL_REGISTER = "http://192.168.1.11/android_api/register.php";
 }
